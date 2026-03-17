c0703c5a71eb32c942b3d457c147952130ec9e97
samuelladinog <samuela.ladinog@autonoma.edu.co>
2026-03-17 17:54:34 -0500

    Update PCOficinaBuilder.java

--- src/main/java/org/juanse/models/PCOficinaBuilder.java
+++ src/main/java/org/juanse/models/PCOficinaBuilder.java
@@ -2,60 +2,94 @@ package org.juanse.models;
 
 import org.juanse.models.Computadora;
 
-// Builder concreto para construir una PC de oficina
-// Implementa la interfaz IComputadoraBuilder con componentes basicos
+/**
+ * Builder concreto para construir una PC de oficina.
+ * Es el obrero especializado que sabe que componentes basicos
+ * necesita una computadora para uso de oficina.
+ */
 public class PCOficinaBuilder implements IComputadoraBuilder {
 
     private Computadora pc;
 
-    // Al crear el builder, inicializamos una PC nueva
+    /**
+     * Constructor que inicializa el builder con una PC nueva.
+     */
     public PCOficinaBuilder() {
         this.reset();
     }
 
-    // Asignamos el procesador de la PC
+    /**
+     * Asigna el procesador a la PC en construccion.
+     * @param procesador nombre del procesador, por ejemplo "Intel Core i3"
+     * @return el mismo builder para encadenar metodos
+     */
     @Override
     public IComputadoraBuilder setProcesador(String procesador) {
         this.pc.procesador = procesador;
         return this;
     }
 
-    // Asignamos la cantidad de RAM
+    /**
+     * Asigna la cantidad de RAM a la PC en construccion.
+     * @param ram cantidad de RAM en GB
+     * @return el mismo builder para encadenar metodos
+     */
     @Override
     public IComputadoraBuilder setRam(int ram) {
         this.pc.ram = ram;
         return this;
     }
 
-    // Asignamos la capacidad de almacenamiento
+    /**
+     * Asigna el almacenamiento a la PC en construccion.
+     * @param almacenamiento capacidad en GB
+     * @return el mismo builder para encadenar metodos
+     */
     @Override
     public IComputadoraBuilder setAlmacenamiento(int almacenamiento) {
         this.pc.almacenamiento = almacenamiento;
         return this;
     }
 
-    // Una PC de oficina generalmente no necesita tarjeta grafica dedicada
+    /**
+     * Indica si la PC tendra tarjeta grafica dedicada.
+     * Para oficina normalmente se envia false.
+     * @param tieneGrafica true si tiene grafica, false si no
+     * @return el mismo builder para encadenar metodos
+     */
     @Override
     public IComputadoraBuilder setGrafica(boolean tieneGrafica) {
         this.pc.tieneGrafica = tieneGrafica;
         return this;
     }
 
-    // Una PC de oficina tampoco necesita luces RGB
+    /**
+     * Indica si la PC tendra luces RGB.
+     * Para oficina normalmente se envia false.
+     * @param tieneLuces true si tiene RGB, false si no
+     * @return el mismo builder para encadenar metodos
+     */
     @Override
     public IComputadoraBuilder setLucesRGB(boolean tieneLuces) {
         this.pc.tieneLucesRGB = tieneLuces;
         return this;
     }
 
-    // Reiniciamos el builder para poder construir otra PC desde cero
+    /**
+     * Reinicia el builder creando una PC nueva desde cero.
+     * @return el mismo builder para encadenar metodos
+     */
     @Override
     public IComputadoraBuilder reset() {
         this.pc = new Computadora();
         return this;
     }
 
-    // Retornamos la PC construida y reseteamos para la proxima construccion
+    /**
+     * Retorna la PC ya construida y reinicia el builder
+     * para que pueda usarse de nuevo.
+     * @return la computadora con todos sus componentes asignados
+     */
     @Override
     public Computadora getComputadora() {
         Computadora resultado = this.pc;
