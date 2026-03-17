58b248a94e5c4c7d568e357b4f8eb9ae07ab510e
samuelladinog <samuela.ladinog@autonoma.edu.co>
2026-03-17 17:07:51 -0500

    Update PCOficinaBuilder.java

--- src/main/java/org/juanse/models/PCOficinaBuilder.java
+++ src/main/java/org/juanse/models/PCOficinaBuilder.java
@@ -2,50 +2,60 @@ package org.juanse.models;
 
 import org.juanse.models.Computadora;
 
+// Builder concreto para construir una PC de oficina
+// Implementa la interfaz IComputadoraBuilder con componentes basicos
 public class PCOficinaBuilder implements IComputadoraBuilder {
 
     private Computadora pc;
 
-    public PCOficinaBuilder(){
+    // Al crear el builder, inicializamos una PC nueva
+    public PCOficinaBuilder() {
         this.reset();
     }
 
+    // Asignamos el procesador de la PC
     @Override
     public IComputadoraBuilder setProcesador(String procesador) {
-        this.pc.procesador= procesador;
+        this.pc.procesador = procesador;
         return this;
     }
 
+    // Asignamos la cantidad de RAM
     @Override
     public IComputadoraBuilder setRam(int ram) {
         this.pc.ram = ram;
         return this;
     }
 
+    // Asignamos la capacidad de almacenamiento
     @Override
     public IComputadoraBuilder setAlmacenamiento(int almacenamiento) {
-        this.pc.almacenamiento=almacenamiento;
+        this.pc.almacenamiento = almacenamiento;
         return this;
     }
 
+    // Una PC de oficina generalmente no necesita tarjeta grafica dedicada
     @Override
     public IComputadoraBuilder setGrafica(boolean tieneGrafica) {
-        this.pc.tieneGrafica=tieneGrafica;
+        this.pc.tieneGrafica = tieneGrafica;
         return this;
     }
 
+    // Una PC de oficina tampoco necesita luces RGB
     @Override
     public IComputadoraBuilder setLucesRGB(boolean tieneLuces) {
-        this.pc.tieneLucesRGB=tieneLuces;
+        this.pc.tieneLucesRGB = tieneLuces;
         return this;
     }
 
+    // Reiniciamos el builder para poder construir otra PC desde cero
     @Override
     public IComputadoraBuilder reset() {
         this.pc = new Computadora();
         return this;
     }
 
+    // Retornamos la PC construida y reseteamos para la proxima construccion
     @Override
     public Computadora getComputadora() {
         Computadora resultado = this.pc;
